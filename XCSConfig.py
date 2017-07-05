f1e3ec92a2e5b5a0a85dbee61c5a78741360b0b6
VicientReno <b60306@hotmail.com>
2017-07-05 09:44:58 +0800

    XCSConfig.py
    Edit parameters and choose the execution mode (Multiplexer or Questionnaire)

---
+++ XCSConfig.py
@@ -0,0 +1,31 @@
+class XCSConfig:
+    k = 2
+    N = 600
+    max_iterations = 3000
+    max_experiments = 10
+    MUX = True
+    File="C:\Users\LY\Desktop\Project\elementary school.csv"
+    
+    alpha = 0.1
+    beta = 0.2
+    gamma = 0.71
+    delta = 0.1
+    myu = 0.04
+    nyu = 5
+    chi = 0.8
+    
+    epsilon_0 = 10
+    
+    theta_ga = 25
+    theta_del = 20
+    theta_sub = 20
+    theta_mna = 2
+    
+    p_sharp = 0.01
+    p_explr = 1.0
+    
+    doGASubsumption =True
+    doActionSetSubsumtion = True
+    
+XCSConfig = XCSConfig()
+conf =XCSConfig
